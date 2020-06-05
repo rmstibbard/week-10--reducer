c0af0d40ea44896ca38a37cf2afda7b5099c494a
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-05 15:55:04 +0100

    TDD - First test Part 2 - GREEN

--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -13,6 +13,8 @@ it('adds items', () => {
     let result = addItem(initialState, { value: "Hello" });
     // first item in the items array should be a new task with "Hello"
     expect(result.items[0]).toEqual({ task: "Hello", completed: false });
+    // shouldn't be the same array we started with
+    expect(result.items).not.toBe(initialState.items);
 });
 
 it('removes items', () => {
