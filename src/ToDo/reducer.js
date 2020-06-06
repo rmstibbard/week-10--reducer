3b1cedd9e546974857f3662304d0fecbf15e22d1
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-06 10:56:42 +0100

    TDD - Copied from answers

--- src/ToDo/reducer.js
+++ src/ToDo/reducer.js
@@ -3,10 +3,10 @@ import React, { useReducer } from 'react';
 export const addItem = (state, { value }) => {
 
     return {
-        ...state,
+        ...state,   // ?Keeps a copy of 'state'?
         items:
             [
-                ...state.items, {
+                ...state.items, {   // Adds item named 'value' to 'state.items'
                     task: value,
                     completed: false
                 }
@@ -14,9 +14,52 @@ export const addItem = (state, { value }) => {
     }
 }
 
-export const removeItem = (state, { index }) => {
+export const removeItem = (state, { index }) => {             // Copied from answers
     return {
-        ...state,                                           // Copied from answers
-        items: state.items.filter((_, i) => i !== index),   // Returns only those items whose index number is
-    };                                                      // not the one passed in
+        ...state,
+        items: state.items.filter((item, i) => i !== index),   // Returns only those items whose index number is
+    };                                                         // not the one passed in
+};
+
+
+
+// ALL JUST COPIED FROM ANSWERS - COULD NOT WRITE THIS MYSELF
+
+export const updateItem = (state, { index, value }) => {        // Copied from answers
+    let items = [...state.items];
+
+    items[index] = {
+        ...state.items[index],
+        task: value
+    };
+
+    return {
+        ...state,
+        items: items,
+    };
+};
+
+
+export const completeItem = (state, { index }) => {
+    let items = [...state.items];
+
+    items[index] = {
+        ...state.items[index],
+        completed: true
+    };
+
+    return {
+        ...state,
+        items: items,
+    };
+};
+
+export default (state, action) => {
+    switch (action.type) {
+        case "NEW_ITEM": return addItem(state, action);
+        case "REMOVE_ITEM": return removeItem(state, action);
+        case "CHANGE_ITEM": return updateItem(state, action);
+        case "MARK_COMPLETED": return completeItem(state, action);
+        default: return state;
+    }
 };
--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -55,17 +55,141 @@ it('removes items', () => {
     // "Hello" should be gone, so should get back "Mum" task
     expect(removed.items[0]).toEqual({ task: "Mum", completed: false });
 
+    // should be 5 items, not 6
+    expect(removed.items.length).toBe(5);
+
+    // use the previously pruned array
+    removed = removeItem(removed, { index: 2 });
+
+    // check that the right task is now in index 2
+    expect(removed.items[2]).toEqual({ task: "You", completed: false });
+
 
 });
 
 it('updates items', () => {
-    // Updating tests here
+    let many = {
+        items: [
+            { task: "Hello", completed: true },
+            { task: "Mum", completed: false },
+        ]
+    };
+
+    // update an item passing the index and value
+    let updated = updateItem(many, { index: 1, value: "Dad" });
+
+    // should have updated the task property
+    expect(updated.items[1]).toEqual({ task: "Dad", completed: false });
+
+
+    // should have updated the task property
+    expect(updated.items[1]).toEqual({ task: "Dad", completed: false });
+
+    // check that it's not the same object being returned
+    expect(updated.items[1]).not.toBe(many.items[1]);
+
+    // update the item at index 0
+    updated = updateItem(many, { index: 0, value: "Good Morning" });
+
+    // check that the completed value is the same
+    expect(updated.items[0]).toEqual({ task: "Good Morning", completed: true });
+
 });
 
+
+
+/*
+* tests for completing an item
+*/
 it('completes items', () => {
-    // Completing tests here
+    let many = {
+        items: [
+            { task: "Hello", completed: true },
+            { task: "Mum", completed: false },
+        ]
+    };
+
+    // complete item at index 1
+    let completed = completeItem(many, { index: 1 });
+
+    // complete the item at index 1
+    expect(completed.items[1]).toEqual({ task: "Mum", completed: true });
+
+    // check that it's not the same object being returned
+    expect(completed.items[1]).not.toBe(many.items[1]);
+
+    // complete item at index 0
+    completed = completeItem(many, { index: 0 });
+
+    // should still be marked as completed
+    expect(completed.items[0]).toEqual({ task: "Hello", completed: true });
 });
 
+/*
+* tests for the reducer
+*/
 it('reduces', () => {
-    // Reducer tests here
+    /*
+    * Unsupported action
+    */
+
+    // pass in a nonsense action
+    let newState = reducer(initialState, { type: "GOTTA_CATCH_EM_ALL" });
+
+    // get back initial
+    expect(newState).toBe(initialState);
+
+
+    /*
+    * New item
+    */
+
+    // use the NEW_ITEM action, passing a value
+    newState = reducer(initialState, { type: "NEW_ITEM", value: "Hello" });
+
+    // make sure it's been added
+    expect(newState.items[0]).toEqual({ task: "Hello", completed: false });
+
+
+    /*
+    * Remove item
+    */
+
+    // ALL JUST COPIED FROM ANSWERS - COULD NOT WRITE THIS MYSELF
+    // add some more items
+    newState = reducer(newState, { type: "NEW_ITEM", value: "Mum" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "How" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "Are" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "You" });
+
+    // check they've been added
+    expect(newState.items.length).toBe(5);
+
+    // remove item at index 2
+    newState = reducer(newState, { type: "REMOVE_ITEM", index: 2 });
+
+    // check index 3 has moved down to index 2
+    expect(newState.items[2]).toEqual({ task: "Are", completed: false });
+
+
+    /*
+    * Change item
+    */
+
+    // update item at index 1
+    newState = reducer(newState, { type: "CHANGE_ITEM", index: 1, value: "Dad" });
+
+    // check it's been changed
+    expect(newState.items[1]).toEqual({ task: "Dad", completed: false });
+
+
+    /*
+    * Complete item
+    */
+
+    // mark index 1 as completed
+    newState = reducer(newState, { type: "MARK_COMPLETED", index: 1 });
+
+    // check it's been updated
+    expect(newState.items[1]).toEqual({ task: "Dad", completed: true });
 });
